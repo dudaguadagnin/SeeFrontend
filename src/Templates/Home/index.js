3936744dc159090087bc81a6665e87d0c7513a6d
Eduarda <dudaguadagnin@unochapeco.edu.br>
2022-08-20 14:26:53 -0300

    adicionando componente de card

--- src/Templates/Home/index.js
+++ src/Templates/Home/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import './index.css'
+import Card from '../../components/molecules/Card'
 
 export const Home = (props) => {
-
   return (
     <div className='home'>
       
---
+++ src/components/atoms/CardTitle/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css'
+
+export const CardTitle = (props) => {
+
+  return (
+    <div className='cardtitle'>
+      <span className='card-text'>{props.children}...</span>
+    </div>
+  );
+}
+
+export default CardTitle;
