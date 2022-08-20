40b55410d2d1229942a5188d19a1f79bccb03573
Eduarda <dudaguadagnin@unochapeco.edu.br>
2022-08-20 14:37:10 -0300

    removendo estilo desnecessario

--- App.js
+++ App.js
@@ -49,7 +49,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#12142B',
-    alignItems: 'center',
-    justifyContent: 'center',
+   // alignItems: 'center',
+    //justifyContent: 'center',
   },
 });
